40cd7bd715bcadda57e72b827fba7d0596929bf4
孙翔宇 <sunxiangyu1991@sina.cn>
2015-07-01 10:44:57 -0700

    Cell Image Download

---
+++ Cell Image Demo/Cell Image Demo/Apps.h
@@ -0,0 +1,13 @@
+//
+//  Apps.h
+//  Cell Image Demo
+//
+//  Created by 孙翔宇 on 15/7/1.
+//  Copyright (c) 2015年 孙翔宇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Apps : NSObject
+
+@end
---
+++ Cell Image Demo/Cell Image Demo/Apps.m
@@ -0,0 +1,13 @@
+//
+//  Apps.m
+//  Cell Image Demo
+//
+//  Created by 孙翔宇 on 15/7/1.
+//  Copyright (c) 2015年 孙翔宇. All rights reserved.
+//
+
+#import "Apps.h"
+
+@implementation Apps
+
+@end
